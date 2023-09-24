cd70114d0a64df9e701c59158f750054f6bf45d7
micael-ortega <micaelortega1@gmail.com>
2023-09-24 03:15:02 -0300

    update: set middleware on main func

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/micael-ortega/events-api/handlers"
+	"github.com/micael-ortega/events-api/internals"
 )
 
 func main() {
 	router := gin.Default()
+	router.Use(internals.CORSMiddleware())
 	router.GET("/course", handlers.GetAllCourses)
 	router.POST("/course", handlers.CreateCourse)
 	router.DELETE("/course", handlers.DeleteCourse)
