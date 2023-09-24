9d705e2fb89dee762fa25b1cb770397f7b7c72ed
micael-ortega <micaelortega1@gmail.com>
2023-09-23 23:07:10 -0300

    fix: fixed logic on adding and creating events

--- models/event.go
+++ models/event.go
@@ -3,11 +3,21 @@ package models
 import "time"
 
 type Event struct {
-	ID         int       `json:"id"`
-	Begin_date time.Time `json:"begin_date"`
-	End_date   time.Time `json:"end_date"`
-	Modality   string    `json:"modality"`
-	Duration   float32   `json:"duration"`
-	Instructor Instructor       `json:"instructor"`
-	Course     Course       `json:"course"`
+	ID         int     `json:"id"`
+	Begin_date string  `json:"begin_date"`
+	End_date   string  `json:"end_date"`
+	Modality   string  `json:"modality"`
+	Duration   float32 `json:"duration"`
+	Instructor int     `json:"instructor_id"`
+	Course     int     `json:"course_id"`
+}
+
+type EventResponse struct{
+	ID         int     `json:"id"`
+	Begin_date time.Time  `json:"begin_date"`
+	End_date   time.Time  `json:"end_date"`
+	Modality   string  `json:"modality"`
+	Duration   float32 `json:"duration"`
+	Instructor Instructor     `json:"instructor"`
+	Course     Course     `json:"course"`
 }
