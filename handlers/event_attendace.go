b99a394bcb406d66ac0b2fb0e83e5fb5b5ec06c0
micael-ortega <micaelortega1@gmail.com>
2023-09-24 03:16:01 -0300

    feat: event attendance handler created

--- handlers/event_attendace.go
+++ handlers/event_attendace.go
@@ -1,1 +1,16 @@
 package handlers
+
+import "github.com/gin-gonic/gin"
+
+func GetAllEventsAttendance(c *gin.Context){
+	//TODO
+}
+func GetEventAttendanceByID(c *gin.Context){
+	//TODO
+}
+func SetEventAttendance(c *gin.Context){
+	//TODO
+}
+func UpdateEventAttendance(c *gin.Context){
+	//TODO
+}
